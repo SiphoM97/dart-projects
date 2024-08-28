2259fb4899011a0932fb945a7493cae20c5757f6
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-08-28 12:57:54 +0200

    #using list data type with Strings

--- helloworld.dart
+++ helloworld.dart
@@ -8,4 +8,10 @@ void main() {
   bool simulation = true;
 
   print("the question we all have is $question, well the secret number of the universe is $secretenumber and deep its $deepersecretenumber, I think its $simulation, we live in a Dream");
+
+  List<String> names = ["Elon","Jeff","sam","Brian"];
+  print("Here is a list of the richest people in world $names");
+  print ("here is the number 1 richest person in world ${names[0]}");
+  print ("here is the number 2 richest person in world ${names[1]}");
+
 }
---
+++ list.dart
@@ -0,0 +1,11 @@
+void main() {
+List<String> Names = ["Azania","Sage","Tshimologo","Mpumi"];
+print("value of names is $Names");
+
+print("Value of names[0] is ${Names[0]}");
+print("Value of names[0] is ${Names[1]}");
+print("Value of names[0] is ${Names[2]}");
+print("Value of names[0] is ${Names[3]}");
+
+print(Names);
+}
