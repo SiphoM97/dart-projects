15fdb4f3ffa3af46cf2d1fdaea1ea9b77493b860
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-08-28 12:58:55 +0200

    #typo udate

--- helloworld.dart
+++ helloworld.dart
@@ -7,7 +7,7 @@ void main() {
   double deepersecretenumber = 9.999;
   bool simulation = true;
 
-  print("the question we all have is $question, well the secret number of the universe is $secretenumber and deep its $deepersecretenumber, I think its $simulation, we live in a Dream");
+  print("the question we all have is $question, well the secret number of the universe is $secretenumber and deeper its $deepersecretenumber, I think its $simulation, we live in a Dream");
 
   List<String> names = ["Elon","Jeff","sam","Brian"];
   print("Here is a list of the richest people in world $names");
