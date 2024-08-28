1c0ed371c31c8e16e91d789eaba714d8e1a7a692
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-08-28 11:51:31 +0200

    # String data type

---
+++ dart data types/string.dart
@@ -0,0 +1,10 @@
+void main() {
+
+  // Declaring the data types and variable
+  String name = "Sipho";
+  String business = "Hoja Digital";
+
+// printing the program
+
+print("My name is $name and I am the founder of $business");
+}
