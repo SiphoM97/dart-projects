462bdf85104f8dba50f6c69580847ab6277bfaa3
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-08-28 18:27:13 +0200

    #using arithmatic operators

---
+++ dart data types/arithmatic.dart
@@ -0,0 +1,15 @@
+void main() {
+int num1 = 23;
+int num2 = 224;
+
+int sum = num1 + num2;
+int subtract = num2-num1;
+int mul = num1*num2;
+double div = num2/num1;
+
+print("The answer is $sum");
+print("The answer is $subtract");
+print("The answer is $mul");
+print("The answer is $div");
+
+}
--- helloworld.dart
+++ helloworld.dart
@@ -16,10 +16,23 @@ void main() {
 
   Map<String, int> namess = {'pete': 45, 'Donald':56, 'Zach':27};
   print(namess);
-  
+
   // Define a string with runes
   String runesString = "Runes in Dart: \u{1F600} \u{1F64B} \u{1F680}";
 
   // Print the string
   print(runesString);
+
+  int num1 = 23;
+int num2 = 224;
+
+int sum = num1 + num2;
+int subtract = num2-num1;
+int mul = num1*num2;
+double div = num2/num1;
+
+print("The answer is $sum");
+print("The answer is $subtract");
+print("The answer is $mul");
+print("The answer is $div");
 }
