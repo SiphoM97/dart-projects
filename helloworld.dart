bb7fd0ee351891a4fb208a59f490e2f32365c424
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-08-28 12:11:31 +0200

    #all the data types, String,num,double and bool.

--- helloworld.dart
+++ helloworld.dart
@@ -1,3 +1,11 @@
 void main() { 
-   print("Hello World, Again."); 
+
+   print("Hello World, Again.");
+
+  String question = "is the world a real place";
+  num secretenumber = 999;
+  double deepersecretenumber = 9.999;
+  bool simulation = true;
+
+  print("the question we all have is $question, well the secret number of the universe is $secretenumber and deep its $deepersecretenumber, I think its $simulation, we live in a Dream");
 }
