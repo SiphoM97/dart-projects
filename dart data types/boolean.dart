fbc9c425a9986cc95698605705aca0ece798d186
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-09-20 21:09:50 +0200

    new bool var

--- dart data types/boolean.dart
+++ dart data types/boolean.dart
@@ -4,4 +4,12 @@ void main() {
   String name = "Sipho";
   print("is $name a millionaire ? $millionaire");
 
+  
+  // this is a true or false statement
+
+  bool isOver18 = true;
+  print(isOver18);
+  print(7 < 8);
+
+
 }
