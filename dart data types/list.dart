5db1e9db454a28dd79b39768bb392aec266dc125
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-09-20 21:17:41 +0200

    new list

--- dart data types/list.dart
+++ dart data types/list.dart
@@ -8,4 +8,14 @@ print("Value of names[0] is ${Names[2]}");
 print("Value of names[0] is ${Names[3]}");
 
 print(Names);
+
+   // use square brackets for listing
+  List myList = [1, 2, 4, "Jackson"];
+  // adding value to the list
+
+  myList.add(67);
+  myList.remove("Jackson");
+  // remove value
+  // myList.remove(4);
+  print(myList); 
 }
