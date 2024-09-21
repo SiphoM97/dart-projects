abf708f748a449581e38278ecd529979b4d428a0
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-09-21 09:40:48 +0200

    more iupdates

---
+++ dart control flow statements/if-else-if-statemenr.dart
@@ -0,0 +1,13 @@
+void main() {
+var age = 70;
+
+if (age > 50){
+  print("He most likely voted ANC");
+}
+else if (age <= 40) {
+print("He most likely voted EFF");
+}
+else {
+  print("He most likely voted DA");
+}
+}
---
+++ dart control flow statements/if-else-statement.dart
@@ -0,0 +1,26 @@
+import 'dart:collection';
+
+void main () {
+
+var age = 54;
+if (age < 30) { 
+  print("This is a young man");
+}
+  else{
+    print("This is a old sack of shit!");
+  } 
+
+  // another bit more complicated use of if else statement
+
+ var ANC = 64;
+ var DA = 23;
+ var EFF = 10;
+
+ if (ANC > DA) {
+  print("ANC has wont the election");
+ }
+ else {
+  print("DA has won the election");
+ }
+}
+
