7693418f7d90814415861fffc8ad2d18407cdf27
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-09-21 07:29:44 +0200

    more arithmetic operrations

--- dart data types/maps.dart
+++ dart data types/maps.dart
@@ -6,4 +6,12 @@ void main() {
     'Mark': 70000000000,
   };
   print("individual $networth");
+
+    // Creating a Map with String keys and int values
+  Map<String, int> ages = {
+    'Alice': 30,
+    'Bob': 25,
+    'Charlie': 35,
+  };
+  print("Ages of students: $ages"); 
 }
