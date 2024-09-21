ef08906d52046e5142423283b968fcb7c61ad4ef
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-09-21 10:05:35 +0200

    more practice

--- dart control flow statements/if-else-if-statemenr.dart
+++ dart control flow statements/if-else-if-statemenr.dart
@@ -1,4 +1,5 @@
 void main() {
+/* 
 var age = 70;
 
 if (age > 50){
@@ -10,4 +11,27 @@ print("He most likely voted EFF");
 else {
   print("He most likely voted DA");
 }
+
+*/
+
+// Write a program that determines what type of clothing someone should wear based on the current temperature.
+
+var temprature = 20;
+
+if (temprature > 30){
+  print("wear shorts and a t-shirt.");
 }
+else if (temprature >=20 && temprature <=29 ) {
+  print("Wear a light jacket.");
+}
+else if (temprature >=10 && temprature <=19) {
+  print("wear a sweater and jeans.");
+}
+else if (temprature >= 0 && temprature <=9) {
+  print("wear a coat and scarf.");
+}
+else if (temprature < 0){
+  print("wear a heavy winter coat and gloves.");
+}
+}
+
