7ef9e5c17f054e326695c8d2916f670357fda6c6
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-09-24 09:02:11 +0200

    functions lessons theory complete

---
+++ Dart concepts/functions/anonoymousFunction.dart
@@ -0,0 +1,17 @@
+void main() {
+  // List of fruits
+  var fruits = ['Apple', 'Banana', 'Cherry', 'Date'];
+
+  // Using an anonymous function with forEach to print each fruit
+  fruits.forEach((fruit) {
+    print(fruit);
+  });
+
+  // List of items
+  var items = ['Laptop', 'Tablet', 'Smartphone', 'Smartwatch'];
+
+  // Using an anonymous function with forEach to print each item
+  items.forEach((item) {
+    print(item);
+  });
+}
