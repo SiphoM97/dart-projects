a45a473045520c53c33e49043016b69b9b7eeb24
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-09-30 18:24:24 +0200

    loop statement

--- conceptPractice/loopingstatements/whileloop1.dart
+++ conceptPractice/loopingstatements/whileloop1.dart
@@ -2,10 +2,27 @@
 While Loop Challenge:
 Write a program that:
 
-Prompts the user to guess a secret number (you can set the secret number to any value, e.g., 7).
-Uses a while loop to keep asking the user to guess the number until they get it right.
-After each incorrect guess, print a hint like "Too low!" or "Too high!" based on their input.
+- Prompts the user to guess a secret number (you can set the secret number to any value, e.g., 7).
+
+- Uses a while loop to keep asking the user to guess the number until they get it right.
+
+- After each incorrect guess, print a hint like "Too low!" or "Too high!" based on their input.
+
 Bonus: Display the number of attempts the user made before guessing correctly.
 
 
 */
+
+import 'dart:io';
+
+void main (){
+
+int secretNumber = 17;
+print("Guess the secret number");
+
+String? EnteredNumber = stdin.readLineSync();
+
+int guessedNumber = int.parse(EnteredNumber!);
+
+
+}
--- dartConcepts/dart control flow statements/while-loopstatement.dart
+++ dartConcepts/dart control flow statements/while-loopstatement.dart
@@ -7,3 +7,4 @@ void main() {
     i++;                      // Increment the index
   }
 }
+
