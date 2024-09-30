d81d0af142e50a4229ef2aafa59918d34a4265a2
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-09-30 13:54:23 +0200

    while loop challenge

---
+++ conceptPractice/loopingstatements/whileloop1.dart
@@ -0,0 +1,11 @@
+/* 
+While Loop Challenge:
+Write a program that:
+
+Prompts the user to guess a secret number (you can set the secret number to any value, e.g., 7).
+Uses a while loop to keep asking the user to guess the number until they get it right.
+After each incorrect guess, print a hint like "Too low!" or "Too high!" based on their input.
+Bonus: Display the number of attempts the user made before guessing correctly.
+
+
+*/
