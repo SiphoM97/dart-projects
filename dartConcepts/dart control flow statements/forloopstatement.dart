82dd815cee1bfdaca6aa4dffcb1c9b924eaabc1b
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-01 18:11:57 +0200

    practice

--- dartConcepts/dart control flow statements/forloopstatement.dart
+++ dartConcepts/dart control flow statements/forloopstatement.dart
@@ -1,22 +1,11 @@
 void main () {
 
   
-  /*
+
     int num = 1;  
     for(num; num<=10; num++)           //for loop to print 1-10 numbers  
     {  
         print(num);     //to print the number  
     }
-    */
-
-
-    int shoeSize = 6;
-    for (var i = shoeSize;i <=8; i ++)
-    print("The shoes is small, this is a child $shoeSize");
-
-     int shoeSize = 6;
-  
-  for (var i = shoeSize; i <= 8; i++) {
-    print("The shoe size is $i. This is a child's size.");
-  }
+    
 }
