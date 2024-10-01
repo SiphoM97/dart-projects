85843af126097f3eebe7a68e308429eb9d95684a
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-01 18:11:42 +0200

    for loop

---
+++ randomProjects/datatypespractice2.dart
@@ -0,0 +1,24 @@
+/* 
+
+Challenge: User Profile Data
+User Details:
+
+Create variables for a user's name, age, and country (use appropriate data types).
+
+- Favorite Colors:
+Create a list of 3 favorite colors and store them in a list.
+Boolean:
+
+- Store a Boolean variable to indicate whether the user is subscribed to a newsletter (true or false).
+Unicode:
+
+- Use a rune to store the Unicode for the first letter of the user's name.
+
+- User Map:
+Store all of the user’s details (name, age, country, favorite colors, and newsletter subscription status) in a map where the keys are descriptive strings and the values are the actual user inputs.
+Print Results:
+
+Finally, print each of these variables along with an explanatory sentence (e.g., “User’s name is John.”).
+
+*/
+
