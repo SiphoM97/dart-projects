f399cc968b5a226dee9594adb90923be06b0d3f1
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-03 17:47:28 +0200

    loops

--- dartConcepts/dart control flow statements/while-loopstatement.dart
+++ dartConcepts/dart control flow statements/while-loopstatement.dart
@@ -3,8 +3,9 @@ void main() {
   int i = 0;            // Initialize index
 
   while (i < list1.length) {  // Loop until i is less than the length of the list
-    print(list1[i]);          // Print the current element at index i
+    print(list1[i]);        // Print the current element at index i
     i++;                      // Increment the index
   }
+  
 }
 
---
+++ randomProjects/loopspractice2.dart
@@ -0,0 +1,44 @@
+/* 
+
+Loop Challenge: Counting and Summing Numbers
+Write a program that:
+
+For Loop:
+Use a for loop to print the numbers from 1 to 5.
+
+For-in Loop:
+Create a list of numbers [10, 20, 30, 40, 50] and use a for-in loop to print each number.
+
+While Loop:
+Use a while loop to keep adding 5 to a starting value of 0, stopping when the value is greater than or equal to 25. Print the value after each addition.
+
+Do-While Loop:
+Use a do-while loop to subtract 2 from 10, and stop when the value becomes less than or equal to 0. Print the value after each subtraction.
+
+ */
+
+void main(){
+// Use a for loop to print the numbers from 1 to 5.
+
+int num = 3;
+
+    for(num; num <= 5; num++) {
+
+
+  print("added 1 to $num");
+}
+
+// Create a list of numbers [10, 20, 30, 40, 50] and use a for-in loop to print each number.
+
+List numbers = [10, 20, 30, 40, 50];
+
+for (var i in numbers ) {
+  print(i * 2);
+}
+
+// Use a while loop to keep adding 5 to a starting value of 0, stopping when the value is greater than or equal to 25. 
+//Print the value after each addition.
+
+
+
+}
