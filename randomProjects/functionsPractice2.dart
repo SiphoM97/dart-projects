6020d75d17b288b080c4a2e35dfb3d1d4581162e
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-03 19:11:29 +0200

    funcrions practice

---
+++ randomProjects/functionsPractice2.dart
@@ -0,0 +1,19 @@
+import 'dart:async';
+
+
+// Function has no Parameters and no return type.
+
+void userGreeting(String name){
+  print("Hello $name");
+
+}
+// function has no pramerters but has a return type.
+
+
+
+void main (){
+
+userGreeting("Josh");
+
+
+}
