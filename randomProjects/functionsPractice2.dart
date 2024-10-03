c42d7c99d6812930604cfcd98babc400a11f1f57
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-03 19:22:32 +0200

    funtions types params and return types

--- randomProjects/functionsPractice2.dart
+++ randomProjects/functionsPractice2.dart
@@ -1,19 +1,36 @@
-import 'dart:async';
+/* 
+Challenge: Simple Calculator with Functions
+You will create four functions, each focusing on one of the four types of functions you've listed:
+
+- No Parameters, No Return Type
+Create a function that prints a welcome message for the user.
+
+- No Parameters, Yes Return Type
+Create a function that returns the value of pi (3.14159).
+
+- Yes Parameters, No Return Type
+Create a function that takes two numbers as parameters and prints their sum.
+
+- Yes Parameters, Yes Return Type
+Create a function that takes two numbers as parameters and returns their product.
 
 
-// Function has no Parameters and no return type.
+ */
 
-void userGreeting(String name){
-  print("Hello $name");
+// No Parameters, No Return Type: Create a function that prints a welcome message for the user.
+import 'dart:io';
 
+void welcomeMessage(){
+  print("Welcome to the planet Sipho");
 }
-// function has no pramerters but has a return type.
 
+// No Parameters, Yes Return Type: Create a function that returns the value of pi (3.14159).
+double pi(){
+  
+}
 
+void main(){
 
-void main (){
-
-userGreeting("Josh");
-
+welcomeMessage();
 
 }
