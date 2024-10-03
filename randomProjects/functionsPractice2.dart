cd396e22d3aeac517838b462571625994c7289bf
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-03 19:37:04 +0200

    excecise done functions

--- randomProjects/functionsPractice2.dart
+++ randomProjects/functionsPractice2.dart
@@ -26,11 +26,32 @@ void welcomeMessage(){
 
 // No Parameters, Yes Return Type: Create a function that returns the value of pi (3.14159).
 double pi(){
-  
+  return 3.14159;
+
+}
+
+// Yes Parameters, No Return Type: Create a function that takes two numbers as parameters and prints their sum.
+
+void numbers(int num1, int num2){
+  print(num1 + num2);
+}
+
+//  Yes Parameters, Yes Return Type: Create a function that takes two numbers as parameters and returns their product.
+int add(int num3, int num4){
+  return num3 + num4;
 }
 
 void main(){
 
-welcomeMessage();
+welcomeMessage(); // printing function 1 (no param no return types)
+double Pi = pi(); // calling and storing function 2 (no param yes retrun type)
+
+print("The value for pi is $Pi"); // printing the function value.
+
+numbers(70, 1234);
+
+int result = add(34, 465); // calling the function
+
+print("the sum of num3 and num4 is: $result"); // printing the function.
 
 }
