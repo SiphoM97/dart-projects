353156c28a5edfebe913ef50b7754580f131e0ef
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-07 08:55:11 +0200

    inheritance

--- dartConcepts/Object orientated programming/polymorphism.dart
+++ dartConcepts/Object orientated programming/polymorphism.dart
@@ -1,12 +1,15 @@
 // Base class
+
 class Animal {
   // Method to be overridden by derived classes
+
   void makeSound() {
     print("Animal makes a sound");
   }
 }
 
 // Derived class Dog that overrides makeSound method
+
 class Dog extends Animal {
   @override
   void makeSound() {
@@ -15,6 +18,7 @@ class Dog extends Animal {
 }
 
 // Derived class Cat that overrides makeSound method
+
 class Cat extends Animal {
   @override
   void makeSound() {
@@ -23,12 +27,15 @@ class Cat extends Animal {
 }
 
 void main() {
+
   // Creating instances of Animal, Dog, and Cat
+
   Animal animal = Animal();
   Dog dog = Dog();
   Cat cat = Cat();
 
   // Calling makeSound method for each instance
+
   animal.makeSound(); // Calls Animal's version
   dog.makeSound();    // Calls Dog's version
   cat.makeSound();    // Calls Cat's version
