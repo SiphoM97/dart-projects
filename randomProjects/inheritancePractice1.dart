83dc8a35bb55ffb4676aa0ba27596106e4c40534
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-07 11:02:04 +0200

    inheritance practice

--- randomProjects/inheritancePractice1.dart
+++ randomProjects/inheritancePractice1.dart
@@ -1,5 +1,5 @@
 
-// declaring class.
+// declaring Dog super class.
 
 class Dog{
   String breed;
@@ -13,17 +13,30 @@ class Dog{
   void displayInfo(){
     print("The dog is a $breed bread");
     print("The name of the owner is: $owner");
-    print("The dog is $age old.");
+    print("The dog is $age years old.");
     print("The dogs gender is: $gender");
   }
 
   
 }
-
+// declaring animal clas subclas
 class Animal extends Dog {
 
     String food;
 
     Animal(String breed,String owner,int age,String gender, this.food) : super(breed,age,owner,gender);
     
+    displayAnimalInfo(){
+      displayInfo(); // call the based class.
+      print("Type of food $food");
+
+    }
   }
+
+ void main() {
+  // Creating an instance of the Animal class
+  Animal animal = Animal("Pitbull", "John", 3, "Male", "Chicken");
+
+  // Displaying animal information
+  animal.displayAnimalInfo();
+}
