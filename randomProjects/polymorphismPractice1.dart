ea3daa47b910d779cc50080d48ad472f60c0a86c
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2024-10-07 16:22:24 +0200

    oop program

---
+++ randomProjects/polymorphismPractice1.dart
@@ -0,0 +1,22 @@
+// base call (superclass).
+
+class Human{
+
+  void walking(){
+    print("walks on 2 legs");
+  }
+
+}
+
+
+
+
+
+void main(){
+// creating instance of human class
+  Human human = Human();
+
+// calling walking method for human.
+  human.walking();
+  
+}
