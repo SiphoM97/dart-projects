48081775f46d6942e98c34dee0c7455f981711a4
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-19 15:05:18 +0200

    Data type String practice

---
+++ refresherStudy/01.dataTypes/String.dart
@@ -0,0 +1,30 @@
+void main() {
+  String Name = "Sipho";
+
+  print(Name);
+
+  // Decalring the data type and variable
+
+  String Personal_Name = "Siphosetu";
+  String Business_Name = "Hoja Digital";
+  String Business_Operation = "Software development";
+
+  //Printing the program
+
+  print(
+      "My name us $Personal_Name and I am the founder of $Business_Name and we provide $Business_Operation");
+
+//Length of the string
+
+  print(Personal_Name.length);
+  print(Business_Name.length);
+  print(Business_Operation.length);
+
+// find  a character in the string
+
+  print(Personal_Name.indexOf("t"));
+  print(Business_Name.indexOf("D"));
+  print(Business_Operation.indexOf("f"));
+
+//
+}
