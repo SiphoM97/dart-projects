bb2ad0acd2a1cc81848272aac46463b186688af9
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-19 18:28:46 +0200

    Numbers refresher

--- dartConcepts/dart data types/arithmatic.dart
+++ dartConcepts/dart data types/arithmatic.dart
@@ -1,46 +1,47 @@
 void main() {
-int num1 = 23;
-int num2 = 224;
+  int num1 = 23;
+  int num2 = 224;
 
-int sum = num1 + num2;
-int subtract = num2-num1;
-int mul = num1*num2;
-double div = num2/num1;
+  int sum = num1 + num2;
+  int subtract = num2 - num1;
+  int mul = num1 * num2;
+  double div = num2 / num1;
 
-print("The answer is $sum");
-print("The answer is $subtract");
-print("The answer is $mul");
-print("The answer is $div");
-
+  print("The answer is $sum");
+  print("The answer is $subtract");
+  print("The answer is $mul");
+  print("The answer is $div");
 
 // Declaring more variables
 
-int a = 10;
-int b = 3;
-double x = 5.5;
-double y = 2.5;
+  int a = 10;
+  int b = 3;
+  double x = 5.5;
+  double y = 2.5;
 
 // performing arithmetic operations.
 
-int addition = a + b;
-int subtraction = a - b;
-double division = a / b;
-int multiplication = a * b;  
-int integerDivision = a ~/ b;
-int modulas = a % b;
+  int addition = a + b;
+  int subtraction = a - b;
+  double division = a / b;
+  int multiplication = a * b;
+  int integerDivision = a ~/ b;
+  int modulas = a % b;
 
 // using double variables
 
-double doubleAddition = x + y;
-double multiplicationDouble = x * y;
+  double doubleAddition = x + y;
+  double multiplicationDouble = x * y;
 
 // printing full sums as results
 
-print("addition (int): $a + $b = $addition");
-print("subtraction (int): $a - $b = $subtraction");
-print("multiplication (int): $a * $b = $multiplication");
-print("division (double): $a / $b = $division");
-print("Interger division (int): $a / $b = $integerDivision");
-print("modulas: $a % $b = $modulas");
+  print("addition (int): $a + $b = $addition");
+  print("subtraction (int): $a - $b = $subtraction");
+  print("multiplication (int): $a * $b = $multiplication");
+  print("division (double): $a / $b = $division");
+  print("Interger division (int): $a / $b = $integerDivision");
+  print("modulas: $a % $b = $modulas");
 
+  print("$a + $b = $doubleAddition");
+  print("object")
 }
