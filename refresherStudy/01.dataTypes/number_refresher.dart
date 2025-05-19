f2fb9f1379767128e892676d33c3750f86d172ed
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-19 17:55:28 +0200

    Numbers refresher

---
+++ refresherStudy/01.dataTypes/number_refresher.dart
@@ -0,0 +1,31 @@
+void main() {
+// declaring variables
+  int Age = 28;
+  int year = 2025;
+  double height = 117.94;
+
+//Printing variables
+  print(Age);
+  print(year);
+  print(height);
+
+// making a sum
+
+  int sum = Age + year;
+  print("I will double my age in $sum");
+
+  double sum2 = height + 3;
+
+  print("If i grew by 3cms i would be $sum2 tall");
+
+  // operations in numbers.
+
+  int birthdate = year - Age; // Subtration
+  int twentyYears = Age + 20; // addition
+  double halfAge = Age / 2; // division
+  // multiplication
+
+  print(birthdate);
+  print("Twenty years frm now I will be $twentyYears");
+  print("Half my age is $halfAge");
+}
