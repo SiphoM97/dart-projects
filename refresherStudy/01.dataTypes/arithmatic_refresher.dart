4649791e017e50ae6d8801e548ec8b1ba9c33e2b
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-20 07:49:15 +0200

    arithmatic refresher.

---
+++ refresherStudy/01.dataTypes/arithmatic_refresher.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Declaring variables
+
+  int num1 = 24;
+  int num2 = 23;
+  double num3 = 7.5;
+  int num4 = 2000;
+
+  // arithmatic operations
+
+  int sum1 = num1 + num2;
+  double sum2 = num4 - num3;
+
+  // printing results
+
+  print(sum1);
+  print(sum2);
+}
