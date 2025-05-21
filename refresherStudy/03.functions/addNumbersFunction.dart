1d4bb4d6ef0a0d1ef292a6973ffc40ff22e23fa2
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-21 17:07:17 +0200

    more basic and numeric function

--- refresherStudy/03.functions/addNumbersFunction.dart
+++ refresherStudy/03.functions/addNumbersFunction.dart
@@ -1,8 +1,15 @@
 void main() {
-  findSum(7000, 234);
+  findAddition(7000, 234);
+
+  findSubtraction(20000, 3000);
 }
 
-void findSum(int num1, int num2) {
+void findAddition(int num1, int num2) {
   int sum = num1 + num2;
   print("The sum of $num1 and $num2 is $sum");
+}
+
+void findSubtraction(int num3, int num4) {
+  int sum1 = num3 - num4;
+  print("The subtration of $num3 to $num4 is $sum1");
 }
