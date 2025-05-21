6fc8e5d7beb62cc3a59bc2caf91ecc5c0977d085
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-21 18:16:17 +0200

    parameter and no return type.

--- dartConcepts/functions/paramNoReturnType.dart
+++ dartConcepts/functions/paramNoReturnType.dart
@@ -1,19 +1,17 @@
-void findSum (int num1, int num2){
+void findSum(int num1, int num2) {
   int sum = num1 + num2;
   print("The sum of $num1 and $num2 is : $sum");
 }
 
-void main (){
-
+void main() {
   findSum(20, 400);
   printName("Jackson");
 }
-
 
 //Here printFullName is a function  With a parameter and no return type function that takes the name of the parameter
 
 // parameters and no return type
 
-void printName( String name ) {
+void printName(String name) {
   print("My name is $name ");
 }
