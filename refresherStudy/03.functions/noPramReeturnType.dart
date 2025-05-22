781214c679d5f218bb9c5bc64fb2adee54d3fa98
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-22 11:16:31 +0200

    noParameters and return types. more

--- refresherStudy/03.functions/noPramReeturnType.dart
+++ refresherStudy/03.functions/noPramReeturnType.dart
@@ -1,9 +1,16 @@
 void main() {
   String UefaChampions = footballTeam();
 
-  print("The current European champions are $UefaChampions");
+  int championsValue = teamValue();
+
+  print(
+      "The current European champions are $UefaChampions and they are valued at $championsValue");
 }
 
 String footballTeam() {
   return "Real Madrid";
+}
+
+int teamValue() {
+  return 100000000;
 }
