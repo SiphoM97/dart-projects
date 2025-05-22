540f749c1f717893d2098e83c536b73d723122e4
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-22 10:54:45 +0200

    parameters and no return type.

--- refresherStudy/03.functions/paramNoReturnType.dart
+++ refresherStudy/03.functions/paramNoReturnType.dart
@@ -1,8 +1,13 @@
 void main() {
   findSum(20, 34);
+  printName("Siphosetu");
 }
 
 void findSum(int num1, int num2) {
   int sum1 = num1 + num2;
   print(sum1);
+}
+
+void printName(String name) {
+  print("My name is $name");
 }
