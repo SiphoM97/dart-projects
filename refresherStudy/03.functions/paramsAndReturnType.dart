54d0c61dea459c3773864b35e30ed1db4d714c15
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-22 12:11:56 +0200

    param and return type. more

--- refresherStudy/03.functions/paramsAndReturnType.dart
+++ refresherStudy/03.functions/paramsAndReturnType.dart
@@ -1,9 +1,14 @@
 void main() {
   String vocations = Career("Software Engineer");
+  int Income = earningPerYear(35000, 12);
 
-  print("My current career is as a $vocations");
+  print("My current career is as a $vocations and I earn $Income");
 }
 
 String Career(String career) {
   return career;
+}
+
+int earningPerYear(int salary, int year) {
+  return salary * year;
 }
