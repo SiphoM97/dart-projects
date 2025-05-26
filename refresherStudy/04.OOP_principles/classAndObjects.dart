da973bd21061a1e8f5cd105d7013fff7757444a9
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-26 15:18:19 +0200

    created and instance of a class

--- refresherStudy/04.OOP_principles/classAndObjects.dart
+++ refresherStudy/04.OOP_principles/classAndObjects.dart
@@ -1,18 +1,18 @@
-void main(
-Student student1 = Student('Sipho', 27, 12);
-
-) {
-  // creating a class
+void main() {
+// Creating an instance of a class.
+  Student student1 = Student('Sipho', 27, 12);
+  student1.displayInfo();
 }
 
+// creating a class
 class Student {
   String name;
   int age;
   int grade;
-}
 
-Student(this.name, this.age, this.grade);
+  Student(this.name, this.age, this.grade);
 
-void displayInfo(){
-  print('Name: $name, Age: $age, Grade: $grade');
+  void displayInfo() {
+    print('Name: $name, Age: $age, Grade: $grade');
+  }
 }
