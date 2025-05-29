15146ace54b3a2b591040bde188927d0ef472396
Sipho Mzayiya <realsiphomzayiya@gmail.com>
2025-05-29 16:23:45 +0200

    refresher folder.

--- dartConcepts/Utilities in dart programme/Asynchronous_Programming.dart
+++ dartConcepts/Utilities in dart programme/Asynchronous_Programming.dart
@@ -1,7 +1,7 @@
 // Simulating a network request or a long-running operation
 Future<String> fetchUserData() async {
   // Simulate a delay
-  await Future.delayed(Duration(seconds: 5));
+  await Future.delayed(Duration(seconds: 2));
   // Return user data after the delay
   return 'User data retrieved successfully';
 }
@@ -9,10 +9,10 @@ Future<String> fetchUserData() async {
 // Main function marked as async to use await inside it
 Future<void> main() async {
   print('Fetching user data...');
-  
+
   // Call fetchUserData() and wait for its completion
   String result = await fetchUserData();
-  
+
   // Print the result once fetchUserData() completes
   print(result);
 }
